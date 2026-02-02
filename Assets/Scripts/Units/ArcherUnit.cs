64262888f9fa1df207808095afb8999f2161b061
SkenorDev <nathanskinner@comcast.net>
2026-02-02 13:08:45 -0800

    Basic augment logic implemented
    a

--- Assets/Scripts/Units/ArcherUnit.cs
+++ Assets/Scripts/Units/ArcherUnit.cs
@@ -2,11 +2,9 @@ using UnityEngine;
 
 public class ArcherUnit : BaseUnit
 {
-<<<<<<< Updated upstream
-    // commented out example code for how to handle projectiles
-    // the prefab for this unit needs a prefab for its projectile assigned in inspector
-    //[Header("Archer Specifics")]
-    ///public GameObject arrowPrefab;
+    // If you want to handle projectiles manually, uncomment and use this:
+    // [Header("Archer Specifics")]
+    // public GameObject arrowPrefab;
 
     protected override void PerformBasicAttack()
     {
@@ -14,28 +12,18 @@ public class ArcherUnit : BaseUnit
         float finalDmg = myData.GetModifiedDamage();
         Debug.Log($"Archer {myData.GetInstanceID()} fires for {finalDmg}");
 
-        //GameObject arrow = Instantiate(arrowPrefab, transform.position, Quaternion.identity);
-        //if (arrow.TryGetComponent(out Projectile proj))
-        //{
-        //    proj.Setup(finalDmg, Vector2.right);
-        //}
+        // Example projectile firing logic:
+        // GameObject arrow = Instantiate(arrowPrefab, transform.position, Quaternion.identity);
+        // if (arrow.TryGetComponent(out Projectile proj))
+        // {
+        //     proj.Setup(finalDmg, Vector2.right);
+        // }
     }
-=======
-     //public UnitInstance myData;
-    // We DO NOT override PerformBasicAttack.
-    // The BaseUnit will see "AttackType.Projectile" in the data, 
-    // grab the arrow sprite from the "ProjectileTemplate" child, 
-    // and fire it automatically.
->>>>>>> Stashed changes
 
     protected override void CastAbility()
     {
-<<<<<<< Updated upstream
-        // implement basic ability logic here
-=======
-
         Debug.Log("Archer uses ability"); 
         
->>>>>>> Stashed changes
+        // implement ability logic here
     }
 }
