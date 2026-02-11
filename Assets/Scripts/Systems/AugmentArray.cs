214cfd5c633576b0ba5311414dc3d2abfd91f2fc
SkenorDev <nathanskinner@comcast.net>
2026-02-11 12:11:03 -0800

    added basic shop integration

--- Assets/Scripts/Systems/AugmentArray.cs
+++ Assets/Scripts/Systems/AugmentArray.cs
@@ -4,5 +4,8 @@ using System;
 [Serializable]
 public class AugmentList
 {
+    public List<Augment> AllAugments = new List<Augment>();
+    public List<Augment> ShopAugments = new List<Augment>();
+    public List<Augment> RoundStartAugments = new List<Augment>();
     public List<Augment> activeAugments = new List<Augment>();
 }
--- Assets/Scripts/Systems/AugmentManager.cs
+++ Assets/Scripts/Systems/AugmentManager.cs
@@ -9,24 +9,81 @@ public class AugmentManager : MonoBehaviour
         ApplyAllAugments();
     }
 
-    public void ApplyAllAugments()
+    public void ApplyAllActiveAugments()
     {
         for (int i = 0; i < augmentList.activeAugments.Count; i++)
         {
             augmentList.activeAugments[i].Apply();
         }
     }
-    public void AddAugment(Augment newAugment)
+        public void ApplyAllRoundStartAugments()
+    {
+        for (int i = 0; i < augmentList.RoundStartAugments.Count; i++)
+        {
+            augmentList.RoundStartAugments[i].Apply();
+        }
+    }
+    public void AddActiveAugment(Augment newAugment)
     {
         augmentList.activeAugments.Add(newAugment);
     }
 
-        public void RemoveAugmentByIndex(int index)
+        public void RemoveActiveAugmentByIndex(int index)
     {
         if (index >= 0 && index < augmentList.activeAugments.Count)
         {
             augmentList.activeAugments.RemoveAt(index);
         }
     }
+<<<<<<< Updated upstream
+=======
+        public void AddRoundStartAugment(Augment newAugment)
+    {
+        augmentList.RoundStartAugments.Add(newAugment);
+    }
+
+        public void RemoveRoundStartAugmentByIndex(int index)
+    {
+        if (index >= 0 && index < augmentList.RoundStartAugments.Count)
+        {
+            augmentList.RoundStartAugments.RemoveAt(index);
+        }
+    }
+       public void AddShopAugment(Augment newAugment)
+    {
+        augmentList.ShopAugments.Add(newAugment);
+    }
+          public void RemoveShopAugmentByIndex(int index)
+    {
+        if (index >= 0 && index < augmentList.ShopAugments.Count)
+        {
+            augmentList.ShopAugments.RemoveAt(index);
+        }
+    }
+           public void AddAllAugment(Augment newAugment)
+    {
+        augmentList.AllAugments.Add(newAugment);
+    }
+          public void RemoveAllAugmentByIndex(int index)
+    {
+        if (index >= 0 && index < augmentList.AllAugments.Count)
+        {
+            augmentList.AllAugments.RemoveAt(index);
+        }
+    }
+      void Update()
+    {
+        //Temp logic REMOVE LATER WHEN INROUND LOGIC IS THERE
+        timer += Time.deltaTime;
+
+        if (timer >= interval)
+        {
+
+            timer = 0f;
+            ApplyAllActiveAugments();
+        }
+    }
+
+>>>>>>> Stashed changes
 }
 
